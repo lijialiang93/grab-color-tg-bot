be1101a18e609eee0969866770b57c14117f89cf
李嘉良 <lijialiang93@gmail.com>
2022-05-05 16:01:12 +0800

    deploy to heorku

---
+++ index.js
@@ -0,0 +1,38 @@
+const TelegramBot = require("node-telegram-bot-api");
+const Process = require("./process");
+
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+const colorNums = 5;
+
+bot.on("photo", async (msg) => {
+  const chatId = msg.chat.id;
+  const fileId = msg.photo[msg.photo.length - 1].file_id;
+
+  const buf = await Process.stream2buffer(bot.getFileStream(fileId));
+  const vector = await Process.getVectors(buf);
+
+  if (vector && vector.length > 0) {
+    colors = await Process.applyKmeans(vector, colorNums);
+  }
+
+  if (colors.length > 0) {
+    colors.sort((a, b) => {
+      return b.percentage - a.percentage;
+    });
+
+    const colorDataImgBuf = Process.createColorDataImage(colors);
+    bot.sendPhoto(chatId, colorDataImgBuf);
+
+    let respStr = "";
+    for (const color of colors) {
+      respStr += `${color.hex}\n`;
+    }
+    bot.sendMessage(chatId, respStr);
+  }
+});
+
+bot.on("text", (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Send a photo to get color analyze");
+});
---
+++ process.js
@@ -0,0 +1,100 @@
+const util = require("util");
+const sharp = require("sharp");
+const kmeans = require("node-kmeans");
+const clusterize = util.promisify(kmeans.clusterize);
+const { createCanvas } = require("canvas");
+
+const imageWidth = 512;
+const colorWidth = 400;
+const hexTextX = 430;
+const offset = 50;
+
+const MAX_HEIGHT = 512;
+
+const stream2buffer = (stream) => {
+  return new Promise((resolve, reject) => {
+    const _buf = [];
+    stream.on("data", (chunk) => _buf.push(chunk));
+    stream.on("end", () => resolve(Buffer.concat(_buf)));
+    stream.on("error", (err) => reject(err));
+  });
+};
+
+const getVectors = async (path) => {
+  const image = sharp(path);
+  const { height, width } = await image.metadata();
+  let rHeight = height;
+  let rWidth = width;
+
+  if (height > MAX_HEIGHT) {
+    rHeight = MAX_HEIGHT;
+    rWidth = parseInt(width / (height / MAX_HEIGHT));
+  }
+
+  const { data } = await sharp(path)
+    .resize(rHeight, rWidth)
+    .raw()
+    .toBuffer({ resolveWithObject: true });
+
+  const pixelArray = new Uint8ClampedArray(data.buffer);
+
+  const offset = 3;
+  const vectors = new Array();
+
+  for (let i = 0; i < pixelArray.length; i += offset) {
+    vectors.push([pixelArray[i], pixelArray[i + 1], pixelArray[i + 2]]);
+  }
+
+  return vectors;
+};
+
+const applyKmeans = async (vectors, k) => {
+  const colors = [];
+  const res = await clusterize(vectors, { k });
+  for (const point of res) {
+    const [r, g, b] = point.centroid;
+    const hex = RGBToHex(Math.floor(r), Math.floor(g), Math.floor(b));
+    const percentage = (point.cluster.length / vectors.length).toFixed(4);
+    colors.push({ hex, percentage });
+  }
+
+  return colors;
+};
+
+const RGBToHex = (r, g, b) => {
+  r = r.toString(16).toUpperCase();
+  g = g.toString(16).toUpperCase();
+  b = b.toString(16).toUpperCase();
+
+  if (r.length == 1) r = "0" + r;
+  if (g.length == 1) g = "0" + g;
+  if (b.length == 1) b = "0" + b;
+
+  return "#" + r + g + b;
+};
+
+const createColorDataImage = (colors) => {
+  const height = offset * colors.length;
+  const canvas = createCanvas(imageWidth, height);
+  const ctx = canvas.getContext("2d");
+
+  //background color
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  for (let i = 0, startY = 0; i < colors.length; i++, startY += offset) {
+    const color = colors[i];
+    ctx.fillStyle = color.hex;
+    ctx.fillRect(0, startY, colorWidth, offset);
+    ctx.font = "14px Helvetica";
+    ctx.fillText(color.hex, hexTextX, startY + 30);
+  }
+  return canvas.toBuffer();
+};
+
+module.exports = {
+  stream2buffer,
+  applyKmeans,
+  getVectors,
+  createColorDataImage,
+};
